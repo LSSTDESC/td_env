f95997593385801e206fae6931b71531fd1c5bcc
Heather Kelly <heather999kelly@gmail.com>
2022-02-09 18:32:36 -0500

    Create install-mpich.sh

---
+++ conda/install-mpich.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export mpich=3.3
+export mpich_prefix=mpich-$mpich
+
+curl -LO https://www.mpich.org/static/downloads/$mpich/$mpich_prefix.tar.gz 
+tar xvzf $mpich_prefix.tar.gz                                      
+cd $mpich_prefix                                                        
+unset F90
+./configure                                                           
+make -j 4                                                               
+make install                                                           
+make clean                                                        
+cd ..                                                              
+rm -rf $mpich_prefix
+/sbin/ldconfig
