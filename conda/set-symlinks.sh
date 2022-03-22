2570b127be4ff1437b00084a5b6ba70af11d3336
Heather Kelly <heather999kelly@gmail.com>
2022-03-22 11:06:52 -0400

    add script to set symllinks

---
+++ conda/set-symlinks.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+linkName=$1
+
+scratchBuildDir=/global/cscratch1/sd/heatherk/td_env-devbuilds
+commonDevBuildDir=/global/common/software/lsst/cori-haswell-gcc/stack/td_env-dev
+commonProdBuildDir=/global/common/software/lsst/cori-haswell-gcc/stack/td_env-prod
+
+if [ "$CI_COMMIT_REF_NAME"="dev" ]];
+then
+    curBuildDir=$commonDevBuildDir/$CI_PIPELINE_ID
+    echo "Dev Install Build: " $curBuildDir
+else 
+    if [[ -z "$CI_COMMIT_TAG" ]];
+    then
+        prodBuildDir=$CI_PIPELINE_ID
+    fi
+    curBuildDir=$commonProdBuildDir/$prodBuildDir
+    echo "Prod Build: " $curBuildDir
+fi
+
+cd $curBuildDir/../
+unlink $linkName
+ln -s $curBuildDir $linkName
+
+
