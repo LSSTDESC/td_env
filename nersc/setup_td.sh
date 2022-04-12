fa5bf3b6981a205feb62c9eacf264603bbbe4f71
Heather Kelly <heather@slac.stanford.edu>
2022-04-12 08:14:47 -0700

    move setup_td_dev to prod

--- nersc/setup_td.sh
+++ nersc/setup_td.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 
-# March 21 2022: HK Moving to new TD directory
+# Feb 19 2022: HK Use native install of LSST Sci Pipelines at NERSC
 # Jan 27 2022: HK update to optionally setup LSST Sci Pipelines
 # Mar 05 2021: define few things for CosmoMC (installed by Vivian)
 # Feb 22 2021: R.Kessler - update & add ENVs for SNANA
@@ -8,36 +9,31 @@
 # Feb 2020: install SNANA on Cori
 #
 
-#shopt -s nocasematch
-SCRIPT=`basename ${BASH_SOURCE[0]}`
+SCRIPT=${BASH_SOURCE[0]}
 
 usage() {  # Function: Print a help message.
   echo -e \\n"Help documentation for ${BOLD}${SCRIPT}"\\n
   echo "Command line switches are optional. The following switches are recognized."
+  echo "-k  --Setup the env without doing module purge."
   echo "-n  --Setup the env without the LSST Sci Pipelines."
+  echo "-s  --Setup the env for shifter."
   exit 0
 }
 
+
 # optional parameters
-# -e setup LD_LIBRARY_PATH to allow emacs to function
-# -p turn off module purge
+# -h help
 # -n Do not setup the LSST Sci Pipelines
 #while getopts e:n: flag
-while getopts "hn" flag
+while getopts "hkns" flag
 do
     case "${flag}" in
-	h) usage;;
-#        k) keepenv=${OPTARG};;
+        h) usage;;
+        k) keepenv=1;;
         n) nolsst=1;;
+        s) shifterenv=1;;
     esac
 done
-
-#if [[ -z $keepenv ]];
-#then
-#  module purge
-#  echo "Purging modules to start with a clean env, you can turn this off by rerunning this script with the -k option"
-#fi
-
 
 
 export TD=/global/cfs/cdirs/lsst/groups/TD
@@ -47,13 +43,13 @@ export TD_SL=${TD}/SL
 export TD_SN=${TD}/SN
 export TD_SOFTWARE=${TD}/SOFTWARE
 
-
-
+if [[ -z "$keepenv" ]] && [[ -z $SHIFTER_RUNTIME ]];
+then
+  module purge
+fi
 
 # setup without LSST Science Pipelines
 # Broken since March 2022 Cori OS Upgrade
-#if [[ "$nolsst" ]];
-#if [[ "$1" == "nolsst" ]];
 if [[ $nolsst ]];
 then
   module unload python
@@ -69,7 +65,7 @@ then
   module load intel/19.1.3.304  # for CosmoMC (Mar 5 2021)
   export CC=gcc
 
-  export COSMOMC_DIR="$TD_SN/CosmoMCBBC"
+  export COSMOMC_DIR="$SN_GROUP/CosmoMCBBC"
   export PATH=$PATH:${COSMOMC_DIR}
 
   # Set up SN python
@@ -91,27 +87,34 @@ then
   # Aug 24 2020 RK - silly hack for CFITSIO
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CFITSIO_DIR/lib
 
+elif [ $shifterenv ] || [ $SHIFTER_RUNTIME ]
+then
+  source /opt/lsst/software/stack/loadLSST.bash
+  setup lsst_distrib
+
 # Setup with LSST Science Pipelines
-elif [ -z "$nolsst" ] 
-#elif [ -z "$1" ]	
+elif [ -z "$nolsst" ]
 then
   echo "Setting up TD env with LSST Science Pipelines"
+  
+  export DESC_TD_INSTALL=/global/common/software/lsst/cori-haswell-gcc/stack/td_env-prod/stable
+  source $DESC_TD_INSTALL/setup_td_env.sh
+  export GSL_DIR=$DESC_TD_INSTALL/conda/envs/$LSST_CONDA_ENV_NAME
+  export CFITSIO_DIR=$DESC_TD_INSTALL/conda/envs/$LSST_CONDA_ENV_NAME
 
-#  module load root/6.18.00-py3
+  #export GSL_DIR=$CONDA_PREFIX
+  #export CFITSIO_DIR=$CONDA_PREFIX
 
-  source /global/common/software/lsst/cori-haswell-gcc/stack/setup_any_stack.sh w_2021_40
+  export PYTHONPATH=$PYTHONPATH:$DESC_TD_INSTALL
 
-  export PYTHONPATH=$PYTHONPATH:/global/common/software/lsst/common/miniconda/sn-sprint-oct21/cvmfs/sw.lsst.eu/linux-x86_64/lsst_distrib/w_2021_40/conda/miniconda3-py38_4.9.2/envs/lsst-scipipe-0.7.0-ext/lib/python3.8/site-packages
+  # SLURM_JOB_ID is set only on compute nodes
+  # DESC_TD_KEEP_MPI will be user-controlled way to keep MPI set up 
+  #if [[ -z "$DESC_TD_KEEP_MPI" && -z "$SLURM_JOB_ID" ]];
+  #then
+  #    export PYTHONSTARTUP=$DESC_TD_INSTALL/nompi4py.py
+  #fi
 
-  export GSL_DIR="/cvmfs/sw.lsst.eu/linux-x86_64/lsst_distrib/w_2021_40/conda/miniconda3-py38_4.9.2/envs/lsst-scipipe-0.7.0-ext"
-
-  export CFITSIO_DIR="/cvmfs/sw.lsst.eu/linux-x86_64/lsst_distrib/w_2021_40/conda/miniconda3-py38_4.9.2/envs/lsst-scipipe-0.7.0-ext"
-
-
-#else
-  #echo $1 "is an invalid option, please provide no parameters or use lsst to set up the LSST Science Pipelines"
 fi
-
 
 # DIA Environment Variables
 
@@ -119,10 +122,8 @@ fi
 # SL Environment Variables
 
 
-
 # SN Environment Variables
-
-export SNANA_DIR="$TD_SOFTWARE/SNANA"   
+export SNANA_DIR="$TD_SOFTWARE/SNANA"
 
 export SNDATA_ROOT="$TD_SN/SNANA/SNDATA_ROOT"
 export SNANA_TESTS="$TD_SN/SNANA/SNANA_TESTS"
@@ -144,5 +145,9 @@ export SBATCH_TEMPLATES="$SNANA_LSST_ROOT/SBATCH_TEMPLATES"
 export SNANA_DEBUG="$SNANA_LSST_USERS/kessler/debug"
 
 export PATH=$PATH:${SNANA_DIR}/bin:${SNANA_DIR}/util:${PIPPIN_DIR}
+
+
+# For GCRCatalogs
+export DESC_GCR_SITE='nersc'
 
 export HDF5_USE_FILE_LOCKING=FALSE
