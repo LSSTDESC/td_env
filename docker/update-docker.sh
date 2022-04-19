6c3a350b904ac9324fcffb0f6756d920837a80af
Heather Kelly <heather999kelly@gmail.com>
2022-04-19 10:19:16 -0400

    add import of ligo.em_bright for now

--- docker/update-docker.sh
+++ docker/update-docker.sh
@@ -14,6 +14,9 @@ conda clean -y -a
 # python -m compileall /opt/lsst/software/stack/conda 
 # Backing off compileall for docker images: https://stackoverflow.com/questions/64808915/should-pycache-folders-be-included-in-production-containers
 
+# Import of ligo.em_bright to cause associated data files to be downloaded into the image
+python -c "import ligo.em_bright"
+
 conda env export --no-builds > /opt/lsst/software/stack/td_env-image-nobuildinfo.yml
 conda env export > /opt/lsst/software/stack/td_env-image.yml
 
