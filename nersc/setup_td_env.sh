481a0e78ab1663b4fc4e7d97f26c55d593bcba15
Heather Kelly <heather999kelly@gmail.com>
2022-12-09 13:54:33 -0500

    define FIRECROWN_DIR

--- nersc/setup_td_env.sh
+++ nersc/setup_td_env.sh
@@ -58,3 +58,4 @@ setup lsst_distrib
 # For cosmosis and firecrown.  Should try to find a better way to set these
 export CSL_DIR=$CONDA_PREFIX/lib/python3.8/site-packages/cosmosis/cosmosis-standard-library
 export FIRECROWN_SITE_PACKAGES=$CONDA_PREFIX/lib/python3.8/site-packages
+export FIRECROWN_DIR=$DESC_LSST_INSTALL_DIR/firecrown
