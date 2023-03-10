401767d1499fdda33f3f59fa4fb20a021960de34
Heather Kelly <heather999kelly@gmail.com>
2023-03-10 13:08:37 -0500

    add --nolocks flag

--- conda/build-nersc.sh
+++ conda/build-nersc.sh
@@ -62,7 +62,7 @@ curl -LO https://ls.st/lsstinstall
 bash ./lsstinstall -X $1 
 
 source ./loadLSST.bash
-eups distrib install -t $1 lsst_distrib
+eups distrib install -t $1 lsst_distrib --nolocks
 
 mamba install -c conda-forge -y mpich=3.4.*=external_*
 
