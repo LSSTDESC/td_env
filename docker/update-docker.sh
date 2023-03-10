ac2939a55a035d6ef878a6c667de96a163a6196b
Heather Kelly <heather999kelly@gmail.com>
2023-03-10 11:50:03 -0500

    include RESSPECT and firecrown examples

--- docker/update-docker.sh
+++ docker/update-docker.sh
@@ -3,13 +3,31 @@
 source /opt/lsst/software/stack/loadLSST.bash
 conda install -c conda-forge -y mamba
 
-# Cori offers mpich 3.3.2
-mamba install -c conda-forge -y mpich=3.3.*=external_*
+mamba install -c conda-forge -y mpich=4.0.3=external_*
 
 mamba install -c conda-forge -y --file ./packlist.txt
 pip install --no-cache-dir -r ./piplist.txt
 
 conda clean -y -a 
+
+# Install bayeSN
+git clone https://github.com/bayesn/bayesn-public
+## Skipping full set up until we deal with the required data files in $PYSYN_CDBS
+
+#Install RESSPECT
+git clone https://github.com/COINtoolbox/resspect
+cd resspect
+python setup.py install
+cd ..
+
+# Grab firecrown source so we have the examples subdirectory
+firecrown_ver=$(conda list firecrown | grep firecrown|tr -s " " | cut -d " " -f 2)
+echo $firecrown_ver
+curl -LO https://github.com/LSSTDESC/firecrown/archive/refs/tags/v$firecrown_ver.tar.gz
+tar xvzf v$firecrown_ver.tar.gz
+# Set up a common directory name without version info to set FIRECROWN_DIR more easily
+ln -s firecrown-$firecrown_ver firecrown
+
 
 # python -m compileall /opt/lsst/software/stack/conda 
 # Backing off compileall for docker images: https://stackoverflow.com/questions/64808915/should-pycache-folders-be-included-in-production-containers
