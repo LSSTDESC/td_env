e25ee90e1bc4b017fe427ec647ce558df39916e2
Heather Kelly <heather999kelly@gmail.com>
2023-04-06 12:32:22 -0400

    add TD_SETUP_COMMAND

--- nersc/setup_td_int.sh
+++ nersc/setup_td_int.sh
@@ -148,6 +148,7 @@ export PIPPIN_DIR="$TD_SOFTWARE/Pippin"
 export SBATCH_TEMPLATES="$SNANA_LSST_ROOT/SBATCH_TEMPLATES"
 export SNANA_DEBUG="$SNANA_LSST_USERS/kessler/debug"
 export SNANA_SETUP_COMMAND="source $TD/setup_td_int.sh"
+export TD_SETUP_COMMAND=$SNANA_SETUP_COMMAND
 export SNANA_IMAGE_DOCKER="lsstdesc/td-env:int"
 
 # Add env var to point to bayeSN install
