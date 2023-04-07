1e5ba404010f777ad7165bdbc38d59d37f9a6281
Heather Kelly <heather999kelly@gmail.com>
2023-04-07 08:39:20 -0400

    update VERSION_LIBPYTHON

--- nersc/setup_td_int.sh
+++ nersc/setup_td_int.sh
@@ -63,7 +63,7 @@ export TD_PUBLIC=/global/cfs/cdirs/lsst/www/DESC_TD_PUBLIC
 
 export PYSYN_CDBS=${TD_SOFTWARE}/bayeSN/synphot/grp/redcat/trds
 
-export VERSION_LIBPYTHON=3.8
+export VERSION_LIBPYTHON=3.10
 
 
 if [[ -z "$keepenv" ]] && [[ -z $SHIFTER_RUNTIME ]];
