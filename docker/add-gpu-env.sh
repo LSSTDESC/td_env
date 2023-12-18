9a4a1ecf13b155155a87332ae617b312ad854f75
Heather Kelly <heather999kelly@gmail.com>
2023-12-18 09:47:52 -0500

    Update add-gpu-env.sh

--- docker/add-gpu-env.sh
+++ docker/add-gpu-env.sh
@@ -9,7 +9,7 @@ conda create -y --name td-gpu python=3.11
 conda activate td-gpu
 
 CONDA_OVERRIDE_CUDA="11.8" mamba install -y "tensorflow==2.14.0=cuda118*" -c conda-forge
-mamba install -y  pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+CONDA_OVERRIDE_CUDA="11.8" mamba install -y  pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 #mamba install -y jaxlib=*=*cuda* jax cuda-nvcc -c conda-forge -c nvidia
 mamba install -c conda-forge -y --file ./condalist_gpu.txt
 pip install --upgrade "jax[cuda11_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
