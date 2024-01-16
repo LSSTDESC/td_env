258becee6686d61ae4f3820abef79856adbf3b27
Heather Kelly <heather999kelly@gmail.com>
2024-01-16 11:30:19 -0500

    set setup command GPU parameter if we are currently using GPU env

--- nersc/setup_td_dev.sh
+++ nersc/setup_td_dev.sh
@@ -236,7 +236,15 @@ export PIPPIN_OUTPUT="$SNANA_SCRATCH/PIPPIN_OUTPUT"
 export PIPPIN_DIR="$TD_SOFTWARE/Pippin"
 export SBATCH_TEMPLATES="$SNANA_LSST_ROOT/SBATCH_TEMPLATES"
 export SNANA_DEBUG="$SNANA_LSST_USERS/kessler/debug"
-export SNANA_SETUP_COMMAND="source $TD/setup_td_dev.sh"
+
+# Assumes that if we setup GPU environment the first time, we want the GPU env for all time in this set of runs
+if [[ -z "$gpuenv" ]]
+then
+    export SNANA_SETUP_COMMAND="source $TD/setup_td_dev.sh"
+else
+    export SNANA_SETUP_COMMAND="source $TD/setup_td_dev.sh -g"
+fi
+
 export TD_SETUP_COMMAND=$SNANA_SETUP_COMMAND
 export SNANA_IMAGE_DOCKER="lsstdesc/td-env:dev"
 
