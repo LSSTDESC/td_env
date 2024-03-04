15d753752b87f58623d35cf46127d5da32e00c39
Heather Kelly <heather999kelly@gmail.com>
2024-03-04 16:37:14 -0500

    Update install-mpich-for-gpu.sh

--- docker/install-mpich-for-gpu.sh
+++ docker/install-mpich-for-gpu.sh
@@ -8,7 +8,8 @@ tar xvzf $mpich_prefix.tar.gz
 cd $mpich_prefix                                                        
 unset F90
 unset F90FLAGS
-./configure -with-device=ch4:ofi                                                          
+# ./configure -with-device=ch4:ofi   
+./configure --disable-wrapper-rpath  --disable-cxx --with-device=ch3 --disable-f08 --disable-collalgo-tests
 #./configure FFLAGS=-fallow-argument-mismatch FCFLAGS=-fallow-argument-mismatch --disable-wrapper-rpath --disable-cxx --with-device=ch3                                                          
 make -j 4                                                               
 make install                                                           
