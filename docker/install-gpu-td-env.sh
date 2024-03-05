faea3d7d0dfdc0c062f0c7fefddc3985faccb131
Heather Kelly <heather999kelly@gmail.com>
2024-03-05 11:30:28 -0500

    add gpu packages too

--- docker/install-gpu-td-env.sh
+++ docker/install-gpu-td-env.sh
@@ -11,6 +11,8 @@ DESC_TD_ENV_GPU_INSTALL_DIR=$1
 setup_conda() {
   source $DESC_TD_ENV_GPU_INSTALL_DIR/etc/profile.d/conda.sh
   conda activate base
+  conda create -y --name td-gpu python=3.11
+  conda activate td-gpu
 }
 
 unset PYTHONPATH
@@ -27,10 +29,24 @@ which python
 #export PATH=$1/bin:$PATH
 echo $DESC_TD_ENV_GPU_INSTALL_DIR
 setup_conda
-mamba install -c conda-forge -y mpich=4.2.0.*=external_*
+mamba install -c conda-forge -y mpich=4.1.2.*=external_*
 which python
 which conda
+CONDA_OVERRIDE_CUDA="11.8" mamba install -y "tensorflow==2.14.0=cuda118*" -c conda-forge
+mamba install -y  pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 mamba install -c conda-forge -y --file $2
+pip install --upgrade "jax[cuda11_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 pip install --no-cache-dir -r $3 
 
+curl -LO https://github.com/bayesn/bayesn/archive/refs/tags/v0.3.1.tar.gz
+tar xzf v0.3.1.tar.gz
+ln -s bayesn-0.3.1 bayesn
+cd bayesn
+python3 -m pip install --no-deps --no-cache-dir
+
 conda clean -y -a 
+
+conda config --set env_prompt "({name})" --env
+
+conda env export --no-builds > $DESC_TD_ENV_GPU_INSTALL_DIR/td_env-nersc-gpu-nobuildinfo.yml
+conda env export > $DESC_TD_ENV_GPU_INSTALL_DIR/td_env-nersc-gpu.yml
