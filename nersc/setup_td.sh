fb84449c01de8ea0f2e08ed368a2df4a1f9cc531
Heather Kelly <heather999kelly@gmail.com>
2026-06-02 07:25:47 -0700

    Update environment variable paths to use 'py' directory

--- nersc/setup_td.sh
+++ nersc/setup_td.sh
@@ -154,10 +154,12 @@ then
 
   export DESC_TD_INSTALL=/global/common/software/lsst/install/td_env/$DESC_TD_TAG
   source $DESC_TD_INSTALL/setup_td_env.sh
+
+  export LSST_CONDA_ENV_NAME=$CONDA_DEFAULT_ENV
     
-  export GSL_DIR=$DESC_TD_INSTALL/conda/envs/$LSST_CONDA_ENV_NAME
-  export CFITSIO_DIR=$DESC_TD_INSTALL/conda/envs/$LSST_CONDA_ENV_NAME
-  export YAML_DIR=$DESC_TD_INSTALL/conda/envs/$LSST_CONDA_ENV_NAME
+  export GSL_DIR=$DESC_TD_INSTALL/py/envs/$LSST_CONDA_ENV_NAME
+  export CFITSIO_DIR=$DESC_TD_INSTALL/py/envs/$LSST_CONDA_ENV_NAME
+  export YAML_DIR=$DESC_TD_INSTALL/py/envs/$LSST_CONDA_ENV_NAME
   export ROOT_DIR=$ROOTSYS
 
   export PYTHONPATH=$PYTHONPATH:$DESC_TD_INSTALL
